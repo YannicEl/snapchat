7aa577294ca02c05521564ea71a0a79c418a2c12
yannic.ellhotka@gmail.com <yannic.ellhotka@gmail.com>
2021-11-16 21:44:55 +0100

    fixed typo

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   publicRuntimeConfig: {
     firebaseConfig: {
       apiKey: process.env.FIREBASE_API_KEY,
-      authDomain: process.env.FIREBASE_AUHT_DOMAIN,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
       projectId: process.env.FIREBASE_PROJECT_ID,
       storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
       messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
